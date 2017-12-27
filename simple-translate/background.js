fde93408e9cbc33f654aa5a7d5583a2e8f87b5ad
def00111 <def00111@users.noreply.github.com>
2017-12-27 13:54:00 +0100

    Update background.js

--- simple-translate/background.js
+++ simple-translate/background.js
@@ -46,7 +46,7 @@ function menuCreate() {
     browser.contextMenus.create({
         id: "translatePage",
         title: browser.i18n.getMessage("translatePageMenu"),
-        contexts: ["all"],
+        contexts: ["page"],
     });
 
     browser.contextMenus.create({

c500d825473038d4156f72ed5f16a22ed65a88c3
Nelson Xiao <30677291+Ne1s07@users.noreply.github.com>
2019-04-12 21:40:33 +0800

    Fix the inserted <div> covering origin content
    When inserting <div> after <body> element, it will create a new <body> to contain the new <div> element. This may cause part of the origin website content to be covered. So just use `beforeend` instead of `afterend` to avoid that.

--- src/content/index.js
+++ src/content/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TranslateContainer from "./components/TranslateContainer";
 
-document.body.insertAdjacentHTML("afterend", "<div id='simple-translate'></div>");
+document.body.insertAdjacentHTML("beforeend", "<div id='simple-translate'></div>");
 ReactDOM.render(<TranslateContainer />, document.getElementById("simple-translate"));

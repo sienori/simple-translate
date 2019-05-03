bd948616cd06c05eba780613692ed923bbd68a1d
sienori <iilll7d6f@yahoo.co.jp>
2019-05-03 17:01:15 +0900

    Fix to call overWriteLogLevel only once

--- src/common/log.js
+++ src/common/log.js
@@ -1,7 +1,11 @@
 import log from "loglevel";
 import { getSettings } from "src/settings/settings";
 
+let isInited = false;
 export const overWriteLogLevel = () => {
+  if (isInited) return;
+  isInited = true;
+
   const originalFactory = log.methodFactory;
   log.methodFactory = (methodName, logLevel, loggerName) => {
     const rawMethod = originalFactory(methodName, logLevel, loggerName);

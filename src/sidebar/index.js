e582e88388c3dab3ee0f4d8d23f0bd4af22fee00
Phạm Nguyên Phương <Phamnguyenphuong@hotmail.com>
2026-03-17 09:49:13 +0700

    feat: Add Firefox sidebar functionality

---
+++ src/sidebar/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PopupPage from "../popup/components/PopupPage";
+import "./styles/SidebarPage.scss";
+
+ReactDOM.render(<PopupPage />, document.getElementById("root"));
